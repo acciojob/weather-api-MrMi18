cbd0500de3d8046c53f5a12c42d42ad63464c505
Mohammad Imran <108978570+MrMi18@users.noreply.github.com>
2025-04-29 15:13:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 //your JS code here. If required.
+const API_URL = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=e467712b257e418838be97cc881a71de";
+let body =  document.getElementsByTagName("body");
+let h =  document.getElementsByTagName("h1");
+let btn = document.getElementsByTagName("button");
+let p = document.createELement("p");
 
-let btn = document.getElementByTagName(h1);
-const fatchData  = () => {
-	const res = fetch()
+const fatchData  = async() => {
+	const res = await fetch(API_URL);
+	const json = await res.json();
+	p.innerText = `Current weather in London: ${json?.weather?.main}`;
+	body.append(p);
+	
 }
-btn.onClick = fatchData();
+
+
+btn.addEventListner("click",fatchData());
