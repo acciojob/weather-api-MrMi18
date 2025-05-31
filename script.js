aade4611450ef6056b6ff308f68f0fb63433c74c
Mohammad Imran <108978570+MrMi18@users.noreply.github.com>
2025-05-31 15:46:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-//your JS code here. If required.
-const API_URL = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=e467712b257e418838be97cc881a71de";
-let body =  document.getElementsByTagName("body");
-let h =  document.getElementsByTagName("h1");
-let btn = document.getElementsByTagName("button");
-let p = document.createELement("p");
+const API_URL =
+  "https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=e467712b257e418838be97cc881a71de";
+let body = document.getElementsByTagName("body")[0];
+let h = document.getElementsByTagName("h1");
+let btn = document.getElementsByTagName("button")[0];
+let p = document.createElement("p");
+console.log(body);
+const fetchData = async () => {
+  const res = await fetch(API_URL);
+  const json = await res.json();
+  console.log(json.weather[0].main);
+  p.innerText = `Current weather in London: ${json.weather[0].main}`;
+  body.append(p);
+};
 
-const fatchData  = async() => {
-	const res = await fetch(API_URL);
-	const json = await res.json();
-	p.innerText = `Current weather in London: ${json?.weather?.main}`;
-	body.append(p);
-	
-}
-
-
-btn.addEventListner("click",fatchData());
+btn.addEventListener("click", fetchData);
